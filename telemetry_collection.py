d8cd93265619e837dd8ea87eb2f64bc8de0b3233
Hongbo Wei <hwei0@berkeley.edu>
2024-11-27 15:22:05 -0800

    git stash

---
+++ telemetry_collection.py
@@ -0,0 +1,272 @@
+
+from typing import Union
+from mininet.node import Host
+from mininet.net import Mininet
+from collections.abc import Collection
+from asyncio import get_event_loop
+import json
+from asyncio import Lock
+from pathlib import Path
+import asyncio
+from ovs.flow.ofp import OFPFlow
+from ovs.flow.decoders import FlowEncoder
+import pandas as pd
+from experiment import ExperimentControlBlock
+
+class TelemetryStruct():
+    def __init__(self, timestamp:float, switch_name:str):
+        self.timestamp = timestamp
+        self.switch_name = switch_name
+
+class CounterStruct(TelemetryStruct):
+    def __init__(self, timestamp:float, switch_name:str, interface_name:str, dir:str, stat_type:str, value:int):
+        super(self, CounterStruct).__init__(timestamp, switch_name)
+        self.interface_name = interface_name
+        self.dir = dir
+        self.stat_type = stat_type
+        self.value = value
+
+    def _drop(self):
+        corrupt_struct = CounterStruct(self.timestamp, self.switch_name, self.interface_name, self.dir, self.stat_type, self.value)
+        corrupt_struct.value = None
+        return corrupt_struct
+class StatusStruct(TelemetryStruct):
+    def __init__(self, timestamp:float, switch_name:str, interface_name:str, status:bool):
+        super(self, StatusStruct).__init__(timestamp, switch_name)
+        self.interface_name = interface_name
+        self.status = status
+
+    def _drop(self):
+        corrupt_struct = StatusStruct(self.timestamp, self.switch_name, self.interface_name, self.status)
+        corrupt_struct.status = None
+        return corrupt_struct
+
+class FlowStruct(TelemetryStruct):
+    def __init__(self, timestamp:float, switch_name:str, flow_cli_output:str):
+        super(self, FlowStruct).__init__(timestamp, switch_name)
+        self.match_entries = {}
+        self.action_entries = {}
+        self.info_entries = {}
+        self.original = flow_cli_output
+
+        ofp_parsing:dict = json.loads(json.dumps(OFPFlow(flow_cli_output).dict(), indent=4, cls=FlowEncoder))
+        if "match" in ofp_parsing:
+            self.match_entries = FlowStruct.flatten_dict(ofp_parsing['match'])
+        if "actions" in ofp_parsing:
+            self.action_entries = FlowStruct.flatten_list(ofp_parsing['actions'], 'action')
+        if "info" in ofp_parsing:
+            self.info_entries = FlowStruct.flatten_dict(ofp_parsing['info'])
+
+        self.interface_name = self.info_entries['']
+
+    def _drop(self):
+        corrupt_struct = FlowStruct(self.timestamp, self.switch_name, self.original)
+        for ofp_dict in [corrupt_struct.match_entries, corrupt_struct.action_entries, corrupt_struct.info_entries]:
+            for k in ofp_dict.keys():
+                ofp_dict[k] = None
+        return corrupt_struct
+
+    @staticmethod 
+    def flatten_dict(d:dict, key_header:str="") -> dict:
+        res_dict = {}
+        for k, v in d.items():
+            new_key = f"{key_header}_{k}" if key_header != "" else str(k) 
+            if isinstance(v, list):
+                for (v_k, v_v) in FlowStruct.flatten_list(v).items():
+                    res_dict[f"{new_key}_{v_k}"] = v_v
+            if isinstance(v, dict):
+                for (v_k, v_v) in FlowStruct.flatten_dict(v).items():
+                    res_dict[f"{new_key}_{v_k}"] = v_v
+            else:
+                res_dict[new_key] = v
+
+        return res_dict
+    
+    @staticmethod
+    def flatten_list(l:list, key_header:str="") -> dict:
+        res_dict = {}
+        for idx, v_entry in enumerate(l):
+            new_key = f"{key_header}{idx}" if key_header != "" else str(idx)
+            if isinstance(v_entry, dict):
+                for (v_entry_k, v_entry_v) in FlowStruct.flatten_dict(v_entry).items():
+                    res_dict[f"{new_key}_{v_entry_k}"] = v_entry_v
+            else:
+                assert not isinstance(v_entry, list)
+                res_dict[new_key] = v_entry
+
+        return res_dict
+
+
+class TelemetryControlBlock():
+    def __init__(self, mininet:Mininet, log_dir:Path):
+        self.host_manager_map:dict = {}
+        self.host_list:Collection[Host] = list(mininet.hosts)
+        self.kill_signal:bool = False # TODO: implement graceful termination...
+        self.lock:Lock = Lock()
+        self.log_dir:Path = log_dir
+
+        self.host_list = list(mininet.hosts)
+        for host in mininet.hosts:
+            # self.host_manager_map[host] = 
+            pass
+
+
+
+    def run_simulation(self):
+        loop = get_event_loop()
+        loop.call_later()
+
+
+class HostTelemetryManager():
+    MAX_NUM_ROWS = 1000
+    def __init__(self, host:Host, experiment_control_block:ExperimentControlBlock, telemetry_control_block:TelemetryControlBlock, log_name_prefix:str, max_rows:int=MAX_NUM_ROWS, min_store_time:int=600):
+        self.filename_prefix = log_name_prefix
+        self.file_counter = 0
+        self.experiment_control_block:ExperimentControlBlock = experiment_control_block
+        self.telemetry_control_block:TelemetryControlBlock = telemetry_control_block
+        self.host:Host = host
+        self.log_struct = TelemetryLogStruct()
+        self.min_store_time = min_store_time
+
+
+    async def append_logs(self, log_struct_list:Collection[Union[CounterStruct, StatusStruct, FlowStruct]]):
+        for struct in log_struct_list:
+            if (isinstance(struct, CounterStruct)):
+                self.log_struct.append_counter(struct)
+            elif isinstance(struct, StatusStruct):
+                self.log_struct.append_status(struct)
+            else:
+                assert isinstance(struct, FlowStruct)
+                self.log_struct.append_flow(struct)
+
+        if self.log_struct.write_to_disk(self.telemetry_control_block.log_dir / self.filename_prefix+int(self.file_counter), self.experiment_control_block.get_experiment_timestamp() - self.min_store_time):
+            self.file_counter += 1
+
+        
+class TelemetryLogStruct():
+    MIN_WRITE_LEN = 500
+    def __init__(self):
+        self.num_entries = 0
+
+        # common
+        self.timestamp_list = []
+        # self.hosts = []
+        self.router_name_list = []
+        self.telemetry_type_list = []
+
+        # interface counter telemetry
+        self.interface_name_list = []
+        self.direction_list = []
+        self.counter_type_list = []
+        self.counter_val_list = []
+
+        # flow table telemetry
+        self.match_dict_list = {}
+        self.action_dict_list = {}
+        self.info_dict_list = {}
+
+    @staticmethod
+    def corrupt_multiply(counter_struct:CounterStruct, factor:float):
+        return CounterStruct(counter_struct.timestamp, counter_struct.switch_name, counter_struct.interface_name, counter_struct.dir, counter_struct.stat_type, counter_struct.value*factor)
+    
+    @staticmethod
+    def corrupt_drop(telemetry_struct:TelemetryStruct):
+        return telemetry_struct.drop()
+
+    def append_counter(self, counter_struct:CounterStruct):
+        self.timestamp_list.append(counter_struct.timestamp)
+        self.router_name_list.append(counter_struct.switch_name)
+        self.telemetry_type_list.append("counter")
+
+        self.interface_name_list.append(counter_struct.interface_name)
+        self.direction_list.append(counter_struct.dir)
+        self.counter_type_list.append(counter_struct.stat_type)
+        self.counter_val_list.append(counter_struct.value)
+
+        for _, v in self.match_dict_list.items():
+            v.append(None)
+
+        for _, v in self.action_dict_list.items():
+            v.append(None)
+
+        for _, v in self.info_dict_list.items():
+            v.append(None)
+
+        self.num_entries += 1
+    def append_status(self, status_struct:StatusStruct):
+        self.timestamp_list.append(status_struct.timestamp)
+        self.router_name_list.append(status_struct.switch_name)
+        self.telemetry_type_list.append("status")
+
+        self.interface_name_list.append(status_struct.interface_name)
+        self.direction_list.append(None)
+        self.counter_type_list.append("iface_status")
+        self.counter_val_list.append(status_struct.status)
+
+        for _, v in self.match_dict_list.items():
+            v.append(None)
+
+        for _, v in self.action_dict_list.items():
+            v.append(None)
+
+        for _, v in self.info_dict_list.items():
+            v.append(None)
+
+        self.num_entries += 1
+    
+    def append_flow(self, flow_struct:FlowStruct):
+        self.num_entries += 1
+        self.timestamp_list.append(flow_struct.timestamp)
+        self.router_name_list.append(flow_struct.switch_name)
+        self.telemetry_type_list.append("flow_entry")
+
+        self.interface_name_list.append(None)
+        self.direction_list.append(None)
+        self.counter_type_list.append(None)
+        self.counter_val_list.append(None)
+
+
+        for log_dict, flow_dict in [(self.match_dict_list, flow_struct.match_entries), (self.action_dict_list, flow_struct.action_entries), (self.info_dict_list, flow_struct.info_entries)]:
+            for k, lst in log_dict.items():
+                if k in flow_dict:
+                    lst.append(flow_dict[k])
+                else:
+                    lst.append(None)
+
+            for k, lst in flow_dict.items():
+                if k not in log_dict:
+                    log_dict[k] = [None for _ in range(self.num_entries)]
+
+
+    def get_idx_before(self, timestamp:int):
+        for i in range(len(self.timestamp_list)):
+            if self.timestamp_list[i] > timestamp:
+                return i - 1
+        return -1
+
+    def write_to_disk(self, path:Path, timestamp_before:int=10e10)->bool:
+        idx_before = self.get_idx_before(timestamp_before)
+        if (idx_before < TelemetryLogStruct.MIN_WRITE_LEN - 1):
+            return False
+        df_dict = {"timestamp": self.timestamp_list[:idx_before], "router_name": self.router_name_list[:idx_before], "telemetry_type":self.telemetry_type_list[:idx_before], "interface_name": self.interface_name_list[:idx_before], "counter_direction": self.direction_list[:idx_before], "counter_type": self.counter_type_list[:idx_before], "counter_val": self.counter_val_list[:idx_before]}
+        for dict_list in [self.info_dict_list, self.match_dict_list, self.action_dict_list]:
+            for k, v in dict_list.items():
+                df_dict[k] = v[:idx_before]
+
+        pd.DataFrame(df_dict).to_csv(path)
+
+        self.timestamp_list = self.timestamp_list[idx_before:]
+        self.router_name_list = self.router_name_list[idx_before:]
+        self.telemetry_type_list = self.telemetry_type_list[idx_before:]
+        self.interface_name_list = self.interface_name_list[idx_before:]
+        self.direction_list = self.direction_list[idx_before:]
+        self.counter_type_list = self.counter_type_list[idx_before:]
+        self.counter_val_list = self.counter_val_list[idx_before:]
+
+        for dict_list in [self.info_dict_list, self.match_dict_list, self.action_dict_list]:
+            for k, v in dict_list.items():
+                dict_list[k] = v[idx_before:]
+
+        return True
+
+
--- topology.py
+++ topology.py
@@ -1,7 +1,103 @@
-import networkx
+from threading import Lock
+import networkx as nx
 import mininet
 from mininet.net import Mininet
+import json
+import importlib
+import topohub.data
+from topohub.mininet import Demands
+import numpy as np
+from mininet.node import Host, Switch
+from networkx.algorithms.components import is_connected
+import asyncio
+import random
+
+class NetworkXTopo(mininet.net.Topo):
+    # TODO: assert that bw, latency arguments are passed correctly
+    def build(self, graph:nx.Graph, *args, **params):
+        self.mn_nx_name_map = {}
+        self.nx_mn_name_map = {}
+        self.nx_graph = graph
+        assert not graph.is_multigraph()
+        self.base_nx_graph = graph.copy()
+
+        for node in graph.nodes:
+            self.addSwitch('s'+str(node), **{k: v for k, v in node.items() if k not in ['id', 'name']})
+            self.mn_nx_name_map['s'+str(node)] = node
+            self.nx_mn_name_map[node] = 's'+str(node)
+
+        for end1, end2 in graph.edges:
+            self.addLink('s'+str(end1), 's'+str(end2), **{k: v for k, v in graph.edges[end1, end2].items() if k not in ['source', 'target']})
+
+        super(NetworkXTopo, self).build(**params)
+
+    @staticmethod
+    def construct_networkx_from_mininet_topo(mn_topo:mininet.net.Topo):
+        nx_topo = mn_topo
+        nx_topo.__class__ = NetworkXTopo # lol
+        nx_topo.mn_nx_name_map = {}
+        nx_topo.nx_mn_name_map = {}
+        nx_topo.nx_graph = nx.Graph()
+
+        for idx, switch in enumerate(mn_topo.switches):
+            nx_topo.mn_nx_name_map[switch.name] = idx
+            nx_topo.nx_mn_name_map[idx] = switch.name
+            nx_topo.nx_graph.add_node(idx)
+
+        for link in mn_topo.links:
+            n1, n2 = link.intf1.node, link.intf2.node
+            if (not isinstance(n1, Switch) or not isinstance(n2, Switch)):
+                continue
+
+            nx_topo.nx_graph.add_edge(nx_topo.mn_nx_name_map[n1.name], nx_topo.mn_nx_name_map[n2.name])
+
+        nx_topo.base_nx_graph = nx_topo.nx_graph.copy()
+        return nx_topo
+
 class TopologyControlBlock():
-    def __init__(self, mininet:Mininet):
-        self.net = mininet
+    AVG_LINK_LATENCY_TARGET = 40
+    def __init__(self, nx_topo:NetworkXTopo, event_interarrival_fn:function):
+        self.nx_topo = nx_topo
+        self.mininet = Mininet(self.nx_topo)
+        self.event_interarrival_fn = event_interarrival_fn
+        assert is_connected(self.nx_topo.nx_graph)
+
+    async def simulate(self):
+        while True:
+            asyncio.sleep(self.event_interrarival_fn())
+
+            edge = random.sample(set([tuple(e) for e in self.nx_topo.base_nx_graph.edges]), 1)
+            n1, n2 = self.mininet.getNodeByName(edge[0]), self.mininet.getNodeByName(edge[1])
+            if (self.nx_topo.nx_graph.has_edge(edge[0], edge[1])):
+                self.nx_topo.nx_graph.remove_edge(edge[0], edge[1])
+                if not is_connected(self.nx_topo.nx_graph):
+                    self.nx_topo.nx_graph.add_edge(edge[0], edge[1])
+                    continue
+                else:
+                    self.mininet.configLinkStatus(n1, n2, "down")
+                    assert not any([link.intf1.isUp() or link.intf2.isUp() for link in self.mininet.linksBetween(n1, n2)])
+            else:
+                self.nx_topo.nx_graph.add_edge(edge[0], edge[1])
+                self.mininet.configLinkStatus(n1, n2, "up")
+                assert all([link.intf1.isUp() and link.intf2.isUp() for link in self.mininet.linksBetween(n1, n2)])
+    @staticmethod
+    def create_mininet_from_topohub(topo_name:str):
+        topo = topohub.get("sndlib/germany50")
+        g:nx.Graph = nx.node_link_graph(topo)
+
         
+    @staticmethod
+    def construct_bw_jitter_from_topohub(graph:nx.Graph):
+        avg_dist = np.mean([edge['dist'] for edge in graph.edges])
+        for end1, end2 in graph.edges:
+            edge = graph.edges[end1, end2]
+            edge['latency'] *= TopologyControlBlock.AVG_LINK_LATENCY_TARGET / avg_dist
+            edge['bw'] = None
+            # TODO
+
+
+    @staticmethod
+    def construct_mininet_from_networkx(graph:nx.Graph, enable_bw:True, enable_latency:True):
+        assert not ((not enable_bw) and enable_latency)
+        # TODO
+
